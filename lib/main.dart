f964e39de477eb466cc4a10141fedb338de34ccc
ReemaAsker <reema1822000@gmail.com>
2026-04-15 18:57:30 +0200

    basic structure files

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [],
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Screen')),
+      body: Center(child: Text('Home Screen')),
+    );
+  }
+}
---
+++ lib/models/category.dart
@@ -0,0 +1,19 @@
+class Category {
+  late String slug;
+  late String name;
+
+  Category({required this.slug, required this.name});
+
+  Category.fromJson(Map<String, dynamic> json) {
+    slug = json['slug'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['slug'] = this.slug;
+    data['name'] = this.name;
+
+    return data;
+  }
+}
